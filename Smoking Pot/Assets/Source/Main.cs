a3788e2e34063ed605ebaafa16c659faa163ffc2
Vladimir Chernov <2chernov@gmail.com>
2016-01-29 23:33:36 +0200

    Core started.

---
+++ Smoking Pot/Assets/Source/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    [Serializable]
+    public class LevelParameters
+    {
+        public float Time;
+        public float Speed;
+    }
+
+    public LevelParameters[] Levels;
+
+    private void Start()
+    {
+    }
+}
