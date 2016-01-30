bea4572ad8bd3d0f0f58d287d107c706bfdb1d09
Joona Pääkkönen <jonah.paakkonen@gmail.com>
2016-01-31 01:16:23 +0200

    Added flickering light to scene

---
+++ Smoking Pot/Assets/Source/LightFlicker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+
+    public float minIntensity = 0.25f;
+    public float maxIntensity = 2.5f;
+    public float speed = 0.05f;
+    double accumulator = 0;
+
+    Light light;
+
+    // Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!light)
+            return;
+
+        accumulator += Time.deltaTime;
+        while (accumulator > speed)
+        {
+            light.intensity = Random.Range(minIntensity, maxIntensity);
+            accumulator -= speed;
+        }
+
+    }
+
+}
