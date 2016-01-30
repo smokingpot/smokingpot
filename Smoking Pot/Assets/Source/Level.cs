31d61144d70d8acf51252800859bb7aaf4c49d89
Vladimir Chernov <2chernov@gmail.com>
2016-01-30 15:18:27 +0200

    Parameters configured.

--- Smoking Pot/Assets/Source/Level.cs
+++ Smoking Pot/Assets/Source/Level.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
+    public class Item
+    {
+        public Sprite Image;
+        public int Amount;
+    }
+
     public Sprite[] Ingredients;
 
     public SpawnPoint[] SpawnPoints
