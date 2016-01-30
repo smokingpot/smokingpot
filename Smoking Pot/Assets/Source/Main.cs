647d79bbdf94a9d5394f28e3b06b44c0415a8647
Vladimir Chernov <2chernov@gmail.com>
2016-01-30 02:11:00 +0200

    Basic UI flow.

--- Smoking Pot/Assets/Source/Main.cs
+++ Smoking Pot/Assets/Source/Main.cs
@@ -13,11 +13,63 @@ public class Main : MonoBehaviour
 
     public LevelParameters[] Levels;
 
+    public Canvas UICanvas;
+
+    public GameObject RecipeWindowPrefab;
+    public GameObject LevelCompletedWindowPrefab;
+
+    private RecipeWindow _recipeWindow;
+
     private void Start()
+    {
+        OpenRecipeWindow();
+    }
+
+    private void LoadLevel(LevelParameters parameters)
     {
     }
 
-    public void LoadLevel(LevelParameters parameters)
+    private T OpenWindow<T>(GameObject prefab) where T : GameWindow
     {
+        GameObject windowObj = Instantiate(prefab);
+        windowObj.transform.SetParent(UICanvas.transform, false);
+        return windowObj.GetComponent<T>();
     }
+
+    private void CloseWindow<T>(ref T window) where T : GameWindow
+    {
+        Destroy(window.gameObject);
+        window = null;
+    }
+
+    #region Recipe Screen
+
+    private void OpenRecipeWindow()
+    {
+        if (_recipeWindow != null)
+        {
+            throw new InvalidOperationException("Recipe window already opened");
+        }
+
+        _recipeWindow = OpenWindow<RecipeWindow>(RecipeWindowPrefab);
+        _recipeWindow.PlayClick += HandlePlayClick;
+    }
+
+    private void CloseRecipeWindow()
+    {
+        if (_recipeWindow == null)
+        {
+            throw new InvalidOperationException("Recipe window is not opened");
+        }
+
+        _recipeWindow.PlayClick -= HandlePlayClick;
+        CloseWindow(ref _recipeWindow);
+    }
+
+    private void HandlePlayClick()
+    {
+        CloseRecipeWindow();
+    }
+
+    #endregion
 }
---
+++ Smoking Pot/Assets/Source/UI/RecipeWindow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class RecipeWindow : GameWindow
+{
+    public event Action PlayClick;
+
+    public void OnPlayClick()
+    {
+        if (PlayClick != null)
+        {
+            PlayClick();
+        }
+    }
+}
