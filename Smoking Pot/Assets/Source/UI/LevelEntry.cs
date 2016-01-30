5bdd471c659d07c40f5f1f46a558ef3e6f3f3efa
Vladimir Chernov <2chernov@gmail.com>
2016-01-30 22:05:36 +0200

    Level selection window.

---
+++ Smoking Pot/Assets/Source/UI/LevelEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class LevelEntry : MonoBehaviour
+{
+    private LevelSelectionWindow _window;
+    private int _levelNumber;
+
+    public void Init(LevelSelectionWindow window, int levelNumber)
+    {
+        _window = window;
+        _levelNumber = levelNumber;
+    }
+
+    public void OnClick()
+    {
+        _window.OnLevelSelected(_levelNumber);
+    }
+}
