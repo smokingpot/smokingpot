049f4280ebf5f8b2026a5c24e8a151c053f2f79d
Mikko Kumara <mikko.kumara@gmail.com>
2016-01-31 13:47:50 +0200

    begin work on UI element that shows collected items after game over

---
+++ Smoking Pot/Assets/Source/UI/CollectedItem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CollectedItem : MonoBehaviour {
+
+	public Image ItemImage;
+	public Text RecipeAmount;
+	public Text CollectedAmount;
+
+	public Sprite Image
+	{
+		set
+		{
+			ItemImage.sprite = value;
+		}
+	}
+
+	public string RecipeAmountText
+	{
+		set
+		{
+			RecipeAmount.text = value;
+		}
+	}
+
+	public string CollectedAmountText
+	{
+		set
+		{
+			CollectedAmount.text = value;
+		}
+	}
+}
