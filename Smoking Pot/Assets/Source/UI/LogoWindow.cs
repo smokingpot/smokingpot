9c58cb126231332b7335883e6e7e535402997c5c
Vladimir Chernov <2chernov@gmail.com>
2016-01-31 13:29:10 +0200

    Logo screen added.

---
+++ Smoking Pot/Assets/Source/UI/LogoWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class LogoWindow : GameWindow
+{
+    public event Action Close;
+
+    public void OnClose()
+    {
+        if (Close != null)
+        {
+            Close();
+        }
+    }
+}
